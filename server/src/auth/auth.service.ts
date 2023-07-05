80e0a6a758ac0610679d068eaac3c4d48999d91c
Nicolas Gasco <nicolas.gasco92@gmail.com>
2023-07-05 23:36:41 +0200

    Init auth/intra/signin endpoint

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,7 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) {}
+
+  signinUser(code: string, state: string) {
+    if (state !== process.env.INTRA_STATE) {
+      throw new BadRequestException('State value does not match');
+    }
+
+    // Get Intra User Token
+
+    // Fetch user from database
+
+    // If user already exists, return it
+
+    // Create it
+
+    return 'Hello World!';
+  }
 }
