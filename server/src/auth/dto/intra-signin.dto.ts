e53cb0ccf6585e3ad8dfe3e1edb5abe976523196
Nicolas Gasco <nicolas.gasco92@gmail.com>
2023-07-06 17:54:20 +0200

    Add configModule for environment variables

--- server/src/auth/dto/intra-signin.dto.ts
+++ server/src/auth/dto/intra-signin.dto.ts
@@ -1,11 +1,15 @@
-import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+import { ConfigService } from '@nestjs/config';
+import { IsString, Matches } from 'class-validator';
+import { configModuleOptions } from 'config/app.config';
+
+const configService = new ConfigService(configModuleOptions);
 
 export class IntraSigninDto {
   @IsString()
   code: string;
 
   @IsString()
-  @Matches(new RegExp(process.env.INTRA_STATE), {
+  @Matches(new RegExp(configService.get<string>('INTRA_STATE')), {
     message: 'State value is not valid',
   })
   state: string;
