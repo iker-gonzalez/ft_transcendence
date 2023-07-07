5525310c6dc0fc3c0b7ff0a4accbf8aa53de4b9f
Nicolas Gasco <nicolas.gasco92@gmail.com>
2023-07-07 12:17:43 +0200

    Add logic to retrieve Intra user token from Intra API

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,12 +3,16 @@ import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { configModuleOptions } from 'config/app.config';
+import { IntraService } from './intra/intra.service';
+import { IntraModule } from './intra/intra.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(configModuleOptions),
-    AuthModule,
     PrismaModule,
+    AuthModule,
+    IntraModule,
   ],
+  providers: [IntraService],
 })
 export class AppModule {}
--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { ConfigModule } from '@nestjs/config';
+import { IntraModule } from 'src/intra/intra.module';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, IntraModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
