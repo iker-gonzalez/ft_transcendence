895304dd9fcb6b80911a5859902e98ceaf564279
Nicolas Gasco <nicolas.gasco92@gmail.com>
2023-07-08 17:14:34 +0200

    Add documentation for /auth/intra/signin endpoint

--- server/src/auth/dto/signin-response.ts
+++ server/src/auth/dto/signin-response.ts
@@ -1,9 +1,17 @@
 import { IsNumber } from 'class-validator';
 import { IntraUserDataDto } from './intra-user-data.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class SigninResponseDto {
+  @ApiProperty({
+    description: 'Number of created resources',
+    example: 1,
+  })
   @IsNumber()
   created: number;
 
+  @ApiProperty({
+    description: 'User data from database',
+  })
   data: IntraUserDataDto;
 }
