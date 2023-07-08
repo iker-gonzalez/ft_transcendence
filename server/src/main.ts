8f541bc54bfd64473679d9d4e76c24a441be3c98
Nicolas Gasco <nicolas.gasco92@gmail.com>
2023-07-08 17:12:52 +0200

    Config Swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+import { swaggerConfig } from 'config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config: Omit<OpenAPIObject, 'paths'> = new DocumentBuilder()
+    .setTitle(swaggerConfig.title)
+    .setDescription(swaggerConfig.description)
+    .setVersion(swaggerConfig.version)
+    .build();
+  SwaggerModule.setup(
+    swaggerConfig.slug,
+    app,
+    SwaggerModule.createDocument(app, config),
+  );
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
