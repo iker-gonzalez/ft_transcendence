97c010ff75b0aa54ca931d80a26cbec1916d74ca
Nicolas Gasco <nicolas.gasco92@gmail.com>
2023-07-08 17:12:52 +0200

    Config Swagger

---
+++ server/config/swagger.config.ts
@@ -0,0 +1,7 @@
+export const swaggerConfig = {
+  title: 'Transcendence API',
+  description:
+    'The API of the BE app required to power the Transcendence 42 project.',
+  version: '1.0',
+  slug: 'api',
+};
