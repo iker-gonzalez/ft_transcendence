133fd2136ae6506a6edb61b134362de27c01247e
Nicolas Gasco <nicolas.gasco92@gmail.com>
2023-07-08 17:14:34 +0200

    Add documentation for /auth/intra/signin endpoint

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -2,12 +2,31 @@ import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { IntraSigninDto } from './dto/intra-signin.dto';
 import { SigninResponseDto } from './dto/signin-response';
+import {
+  ApiBadRequestResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('intra/signin')
+  @ApiOperation({
+    summary:
+      'Signin user with 42 Intra API. Gets a 42 Intra code as input. Retrieves user data from 42 Intra API and creates a new user if needed.',
+  })
+  @ApiOkResponse({
+    description:
+      'Returns user data from database. Either existing or new user.',
+    type: SigninResponseDto,
+  })
+  @ApiBadRequestResponse({
+    description: 'State value does not match or provided code is invalid.',
+  })
   @HttpCode(200)
   signinUser(
     @Body() intraSigninDto: IntraSigninDto,
