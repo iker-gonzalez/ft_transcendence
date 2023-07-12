9151e3ee5a1eadd8ecb2650f5d80259c1e78df9f
Nicolas Gasco <nicolas.gasco92@gmail.com>
2023-07-12 08:07:47 +0200

    Add new users/avatar PATCH endpoint

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -2,10 +2,15 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { PrismaService } from '../prisma/prisma.service';
 import { UpdateNameDto } from './dto/update-name.dto';
+import { MulterFileDto } from './dto/multer-file.dto';
+import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
 
 @Injectable()
 export class UserService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly cloudinaryService: CloudinaryService,
+    private readonly prisma: PrismaService,
+  ) {}
 
   async updateUsername(
     user: User,
@@ -39,5 +44,43 @@ export class UserService {
       updated: 1,
       data: newUserData,
     };
+  }
+
+  async updateAvatar(user: User, file: MulterFileDto): Promise<UpdateNameDto> {
+    if (!file) {
+      throw new BadRequestException();
+    }
+
+    const { mimetype, size } = file;
+
+    const userData = await this.prisma.user.findUnique({
+      where: {
+        id: user.id,
+      },
+    });
+    const isImage = mimetype.startsWith('image/');
+    const isSizeValid = size >= 50000 && size <= 5000000;
+
+    if (!userData || !isImage || !isSizeValid) {
+      throw new BadRequestException();
+    }
+
+    const uploadPromise = this.cloudinaryService.uploadImage(
+      file,
+      user.username,
+    );
+
+    const { secure_url: avatarUrl }: any = await uploadPromise;
+
+    const newUserData = await this.prisma.user.update({
+      where: {
+        id: user.id,
+      },
+      data: {
+        avatar: avatarUrl,
+      },
+    });
+
+    return { updated: 1, data: newUserData };
   }
 }
