47c641d3a99cd787226b7fbc7e65d9ecc87a2efa
Nicolas Gasco <nicolas.gasco92@gmail.com>
2023-07-30 20:29:53 +0200

    Add second test user

--- server/config/app.constants.ts
+++ server/config/app.constants.ts
@@ -4,3 +4,10 @@ export const testUserData = {
   email: 'test@42urduliz.com',
   username: 'test-',
 };
+
+export const testUser2Data = {
+  avatar: 'http://placekitten.com/500/500',
+  intraId: 667,
+  email: 'test2@42urduliz.com',
+  username: 'test2-',
+};
