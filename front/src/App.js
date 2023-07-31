6719f6425fe01fab3638518024594ab740e3c7d2
iker-gonzalez <ikergpe@gmail.com>
2023-07-31 18:58:15 +0000

    remove unnecesary TS template files

---
+++ front/src/App.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var react_1 = require("react");
+function App() {
+    const onClickButton = () => alert("Button clicked");
+    const contentStyle = {
+        color: "#00babc",
+        fontSize: "20px",
+      };
+    return (
+        <>
+            <h1>Hello World!</h1>
+            <p style={contentStyle}>42Urduliz : Spain</p>
+            <button onClick={onClickButton}>Click here:</button>
+        </>
+    );
+}
+exports.default = App;
--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,31 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-<<<<<<< HEAD:front/src/App.tsx
-          Edit <code>src/App.tsx</code> and save to reload.
-=======
-          Welcome to <code>ft_transcendence</code>project made by<br />
-          @ikgonzal<br /> @ngasco<br />@zcanales<br />@dgerwig
->>>>>>> c3ba6610c13dd6634f7ef495dc9404957c38299e:front/src/App.js
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
