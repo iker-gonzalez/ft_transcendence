3c25dfb48c08fd703a33bc3d4d4aee0043c9dcbf
iker-gonzalez <ikergpe@gmail.com>
2023-08-01 11:47:51 +0000

    compilation errors and warnings fixed

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
 function App() {
-  return <div></div>;
+    const onClickButton = () => alert("Button clicked");
+    const contentStyle = {
+      color: "#00babc",
+      fontSize: "20px",
+    };
+    return (
+    <>
+     <h1>Hello World!</h1>
+        <p style={{ color: "#00babc", fontSize: "20px" }}>42Tokyo : Japan</p>
+        <p style={contentStyle}>42Tokyo : Japan</p>
+     <button onClick={onClickButton}>Click here</button>
+   </>
+    );   
 }
 
 export default App;
