0229567a3086e239c77c4b022f48af291a72abcf
iker-gonzalez <ikergpe@gmail.com>
2023-08-02 11:37:23 +0000

    dependencies updated and second test module added

---
+++ front/src/components/MyFisrtComponent.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function MyFirstComponent() {
+    const contentStyle = {
+        color: "#00babc",
+        fontSize: "20px",
+      };
+      return (
+        <>
+          <p style={contentStyle}>42Tokyo : Japan</p>
+        </>
+      );
+}
+
+export default MyFirstComponent;
