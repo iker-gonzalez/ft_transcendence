12f28449581224b56758c108cdb99e96a496fad5
iker-gonzalez <ikergpe@gmail.com>
2023-08-04 10:05:15 +0000

    JS compilation ready from TSX

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -5,10 +5,10 @@ function App() {
     //const onClickButton = () => alert("Button clicked");
     return (
     <>
-     <h1>42 Pong Game</h1>
-     <h2>Sign In</h2>
-    <SignIn />
-   </>
+        <h1>42 Pong Game!!!!</h1>
+        <h2>Sign In</h2>
+        <SignIn />
+    </>
     );   
 }
 
