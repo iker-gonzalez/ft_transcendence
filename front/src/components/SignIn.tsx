18c40ff83075de400967b83efd02a9f6981b0151
Iker González <84924263+iker-gonzalez@users.noreply.github.com>
2023-08-14 16:36:56 +0000

    SignIn Component UI finished

--- front/src/components/SignIn.tsx
+++ front/src/components/SignIn.tsx
@@ -4,7 +4,16 @@ interface SignInProps {
   onSignIn: (username: string, password: string) => void;
 }
 
+
 function SignIn({ onSignIn }: SignInProps) {
+  const formStyle: React.CSSProperties = {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      //minHeight: '100vh' /* Optional: Ensure the container takes up the full viewport height */
+  };
+
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
@@ -16,27 +25,33 @@ function SignIn({ onSignIn }: SignInProps) {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="username">Username:</label>
-        <input
-          type="text"
-          id="username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-      <button type="submit">Sign In</button>
-    </form>
+    <div style={formStyle}>
+      <h1 style={{fontFamily:'Arial Black', fontSize: '36px', color: 'white'}}>Pong Game</h1>
+      <img src="/assets/school_42.jpeg" alt='42 logo' style={{width: '150px', marginBottom: '12px'}} />
+      <form onSubmit={handleSubmit}>
+        <div style={{width: '150px', marginBottom: '20px'}}>
+          <label htmlFor="username"></label>
+          <input
+            type="text"
+            placeholder='username'
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div style={{width: '150px', marginBottom: '20px'}}>
+          <label htmlFor="password"></label>
+          <input
+            type="password"
+            placeholder='password'
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button style={{width: '175px'}} type="submit">Sign In</button>
+      </form>
+    </div>
   );
 }
 
