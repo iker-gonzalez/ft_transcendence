0eaf5dc2340d50685f2e58bc30f0571a76d16528
Nicolas Gasco <nicolas.gasco92@gmail.com>
2023-08-15 22:57:10 +0200

    Add new autogenerated documentation for matchmaking endpoints

--- server/src/matchmaking/matchmaking.gateway.ts
+++ server/src/matchmaking/matchmaking.gateway.ts
@@ -7,6 +7,10 @@ import {
 import { Server } from 'socket.io';
 import { MatchmakingService } from './matchmaking.service';
 import { Socket } from 'socket.io-client';
+import { AsyncApiPub, AsyncApiSub } from 'nestjs-asyncapi';
+import { NewQueuedUserDto } from './dto/new-queued-user.dto';
+import { NewQueuedUserResponseDto } from './dto/new-queued-user-response.dto';
+import { swaggerAsyncConstants } from 'config/swagger-async.constants';
 
 @WebSocketGateway({
   namespace: 'matchmaking',
@@ -17,6 +21,22 @@ export class MatchmakingGateway implements OnGatewayDisconnect {
   @WebSocketServer()
   server: Server;
 
+  @AsyncApiPub({
+    channel: swaggerAsyncConstants.matchmaking.endpoints.newUser.channel,
+    description:
+      swaggerAsyncConstants.matchmaking.endpoints.newUser.description,
+    message: {
+      payload: NewQueuedUserDto,
+    },
+  })
+  @AsyncApiSub({
+    channel: swaggerAsyncConstants.matchmaking.endpoints.newUserIntraId.channel,
+    description:
+      swaggerAsyncConstants.matchmaking.endpoints.newUserIntraId.description,
+    message: {
+      payload: NewQueuedUserResponseDto,
+    },
+  })
   @SubscribeMessage('newUser')
   onNewUser(client: Socket, data: string): Promise<void> {
     return this.matchmakingService.addUserToQueue(this.server, client.id, data);
