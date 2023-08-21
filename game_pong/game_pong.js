d6f743c5a4cdb446097b63173b260c3bf2c69278
Diego Gerwig <dgerwig-@student.42urduliz.com>
2023-08-21 19:56:23 +0200

    Passing arguments to functions

--- game_pong/game_pong.js
+++ game_pong/game_pong.js
@@ -17,130 +17,108 @@ import {
   initializeSounds,
 } from "./game_pong.functions.js";
 
+const fps = 50;
+
 (async () => {
   const canvas = document.getElementById("gamePong");
 
-  function game() {
+  function game(ballData, user1, bot, match_finish, match_points, sounds) {
     if (!match_finish) {
-      requestAnimationFrame(game);
-      match();
-      render();
+      setTimeout(() => {
+        requestAnimationFrame(function () {
+          game(ballData, user1, bot, match_finish, match_points, sounds);
+        });
+      }, 1000 / fps);
+
+      match(ballData, user1, bot, sounds);
+
+      render(ballData, user1, bot, match_finish, match_points);
     }
   }
 
-  function match() {
-    ball.x += ball.velocityX;
-    ball.y += ball.velocityY;
+  function match(ballData, user1, bot, sounds) {
+    ballData.x += ballData.velocityX;
+    ballData.y += ballData.velocityY;
 
-    if (user1.y < thickness + ball.radius * slit) {
-      user1.y = thickness + ball.radius * slit;
+    if (user1.y < thickness + ballData.radius * slit) {
+      user1.y = thickness + ballData.radius * slit;
     } else if (
       user1.y >
-      canvas.height - thickness - user1.height - ball.radius * slit
+      canvas.height - thickness - user1.height - ballData.radius * slit
     ) {
-      user1.y = canvas.height - thickness - user1.height - ball.radius * slit;
+      user1.y =
+        canvas.height - thickness - user1.height - ballData.radius * slit;
     }
 
-    bot.y += (ball.y - (bot.y + bot.height / 2)) * 0.1;
-    if (bot.y < thickness + ball.radius * slit) {
-      bot.y = thickness + ball.radius * slit;
+    bot.y += (ballData.y - (bot.y + bot.height / 2)) * 0.1;
+    if (bot.y < thickness + ballData.radius * slit) {
+      bot.y = thickness + ballData.radius * slit;
     } else if (
       bot.y >
-      canvas.height - thickness - bot.height - ball.radius * slit
+      canvas.height - thickness - bot.height - ballData.radius * slit
     ) {
-      bot.y = canvas.height - thickness - bot.height - ball.radius * slit;
+      bot.y = canvas.height - thickness - bot.height - ballData.radius * slit;
     }
 
     if (
-      ball.y - ball.radius - thickness < 0 ||
-      ball.y + ball.radius + thickness > canvas.height
+      ballData.y - ballData.radius - thickness < 0 ||
+      ballData.y + ballData.radius + thickness > canvas.height
     ) {
-      ball.velocityY = -ball.velocityY;
-      wall.play();
+      ballData.velocityY = -ballData.velocityY;
+      sounds.wall.play();
     }
 
-    if (ball.x + ball.radius < 0 && !ball.reset) {
+    if (ballData.x + ballData.radius < 0 && !ballData.reset) {
       bot.score++;
-      botScore.play();
-      resetBall();
-    } else if (ball.x - ball.radius > canvas.width && !ball.reset) {
+      sounds.botScore.play();
+      let { newBallData, newUserData } = resetBall(ballData, user1);
+      ballData = newBallData;
+      user1 = newUserData;
+    } else if (ballData.x - ballData.radius > canvas.width && !ballData.reset) {
       user1.score++;
-      userScore.play();
-      resetBall();
+      sounds.userScore.play();
+      let { newBallData, newUserData } = resetBall(ballData, user1);
+      ballData = newBallData;
+      user1 = newUserData;
     }
 
-    let player = ball.x + ball.radius < canvas.width / 2 ? user1 : bot;
+    let player = ballData.x + ballData.radius < canvas.width / 2 ? user1 : bot;
 
-    if (checkCollision(ball, player)) {
-      hit.play();
-      let collidePoint = ball.y - (player.y + player.height / 2);
+    if (checkCollision(ballData, player)) {
+      sounds.hit.play();
+      let collidePoint = ballData.y - (player.y + player.height / 2);
       collidePoint = collidePoint / (player.height / 2);
 
       let angleRad = (Math.PI / 4) * collidePoint;
 
-      let direction = ball.x + ball.radius < canvas.width / 2 ? 1 : -1;
-      ball.velocityX = direction * ball.speed * Math.cos(angleRad);
-      ball.velocityY = ball.speed * Math.sin(angleRad);
+      let direction = ballData.x + ballData.radius < canvas.width / 2 ? 1 : -1;
+      ballData.velocityX = direction * ballData.speed * Math.cos(angleRad);
+      ballData.velocityY = ballData.speed * Math.sin(angleRad);
 
-      ball.speed += 0.1;
+      ballData.speed += 0.1;
       user1.height -= 2;
     }
   }
 
-  function onKeyDown(event) {
-    if (event.keyCode === 38) {
-      // UP ARROW key
-      user1.y -= userSpeedInput * 5;
-    } else if (event.keyCode === 40) {
-      // DOWN ARROW key
-      user1.y += userSpeedInput * 5;
-    }
+  function resetBall(ballData, user1) {
+    const newBallData = ballData;
+    const newUserData = user1;
+    newBallData.reset = true;
 
-    if (event.keyCode === 27) {
-      // ESCAPE key
-      alert("🚦 PAUSE");
-    }
-  }
-
-  function getMousePos(event) {
-    let rect = canvas.getBoundingClientRect();
-    user1.y = event.clientY - rect.top - user1.height / 2;
-    if (user1.y < thickness + ball.radius * slit) {
-      user1.y = thickness + ball.radius * slit;
-    } else if (
-      user1.y >
-      canvas.height - thickness - user1.height - ball.radius * slit
-    ) {
-      user1.y = canvas.height - thickness - user1.height - ball.radius * slit;
-    }
-  }
-
-  function getTouchPos(event) {
-    user1.y = event.clientY - user1.height / 2;
-    if (user1.y < thickness + ball.radius * slit) {
-      user1.y = thickness + ball.radius * slit;
-    } else if (
-      user1.y >
-      canvas.height - thickness - user1.height - ball.radius * slit
-    ) {
-      user1.y = canvas.height - thickness - user1.height - ball.radius * slit;
-    }
-  }
-
-  function resetBall() {
-    ball.reset = true;
     setTimeout(() => {
-      ball.x = canvas.width / 2;
-      ball.y = canvas.height / 2;
-      ball.velocityX = ball.velocityX;
-      ball.velocityY = -ball.velocityY * Math.random(1);
-      ball.speed = userSpeedInput;
-      user1.height = 100;
-      ball.reset = false;
+      newBallData.x = canvas.width / 2;
+      newBallData.y = canvas.height / 2;
+      newBallData.velocityX = newBallData.velocityX;
+      newBallData.velocityY = -newBallData.velocityY * Math.random(1);
+      newBallData.speed = userSpeedInput;
+      newUserData.height = 100;
+      newBallData.reset = false;
     }, 1500);
+
+    return { newBallData, newUserData };
   }
 
-  function render() {
+  function render(ballData, user1, bot, match_finish, match_points) {
     drawRect(0, 0, canvas.width, canvas.height, "Black");
 
     drawText(
@@ -206,7 +184,7 @@ import {
     }
 
     drawText(
-      "Ball Speed: " + ball.speed,
+      "Ball Speed: " + ballData.speed,
       (canvas.width / 10) * 7,
       (canvas.height / 20) * 17,
       "15px Arial",
@@ -230,25 +208,66 @@ import {
 
     drawRect(bot.x, bot.y, bot.width, bot.height, bot.color);
 
-    drawArc(ball.x, ball.y, ball.radius, ball.color);
+    drawArc(ballData.x, ballData.y, ballData.radius, ballData.color);
   }
 
-  // Logic
-  let match_points = 500;
-  let match_finish = false;
-  let ball;
+  (async () => {
+    // Logic
+    let match_points = 500;
+    let match_finish = false;
 
-  const { hit, wall, userScore, botScore } = initializeSounds();
+    const { hit, wall, userScore, botScore } = initializeSounds();
+    const sounds = {
+      hit,
+      wall,
+      userScore,
+      botScore,
+    };
 
-  document.addEventListener("keydown", onKeyDown);
-  canvas.addEventListener("mousemove", getMousePos);
-  canvas.addEventListener("touchmove", getTouchPos);
+    document.addEventListener("keydown", function (event) {
+      if (event.keyCode === 38) {
+        // UP ARROW key
+        user1.y -= userSpeedInput * 5;
+      } else if (event.keyCode === 40) {
+        // DOWN ARROW key
+        user1.y += userSpeedInput * 5;
+      }
 
-  let res = await initializeSessionInDb(ballData, user1, user2);
+      if (event.keyCode === 27) {
+        // ESCAPE key
+        alert("🚦 PAUSE");
+      }
+    });
+    canvas.addEventListener("mousemove", function (event) {
+      let rect = canvas.getBoundingClientRect();
+      user1.y = event.clientY - rect.top - user1.height / 2;
+      if (user1.y < thickness + ballData.radius * slit) {
+        user1.y = thickness + ballData.radius * slit;
+      } else if (
+        user1.y >
+        canvas.height - thickness - user1.height - ballData.radius * slit
+      ) {
+        user1.y =
+          canvas.height - thickness - user1.height - ballData.radius * slit;
+      }
+    });
+    canvas.addEventListener("touchmove", function (event) {
+      user1.y = event.clientY - user1.height / 2;
+      if (user1.y < thickness + ballData.radius * slit) {
+        user1.y = thickness + ballData.radius * slit;
+      } else if (
+        user1.y >
+        canvas.height - thickness - user1.height - ballData.radius * slit
+      ) {
+        user1.y =
+          canvas.height - thickness - user1.height - ballData.radius * slit;
+      }
+    });
 
-  ball = res.data.ball;
+    let res = await initializeSessionInDb(ballData, user1, user2);
 
-  console.log("Ball data:", ball);
+    const ball = res.data.ball;
 
-  game();
+    game(ball, user1, bot, match_finish, match_points, sounds);
+  })();
 })();
