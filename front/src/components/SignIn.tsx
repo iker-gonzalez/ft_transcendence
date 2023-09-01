0bc5bc6c8a4498ede4d025e4adc42826818c2161
Iker González <84924263+iker-gonzalez@users.noreply.github.com>
2023-09-01 17:26:35 +0000

    Fetch API request logic to auth BE service established

--- front/src/components/SignIn.tsx
+++ front/src/components/SignIn.tsx
@@ -1,36 +1,55 @@
 import React, { useState } from 'react';
 
-interface SignInProps {
-  onSignIn: (username: string, password: string) => void;
-}
+const formStyle: React.CSSProperties = {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+};
 
+function SignIn() {
+  const [message, setMessage] = useState<string | null>(null);
 
-function SignIn({ onSignIn }: SignInProps) {
-  const formStyle: React.CSSProperties = {
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      //minHeight: '100vh' /* Optional: Ensure the container takes up the full viewport height */
-  };
-
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
-  function handleSubmit(e: React.FormEvent) {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    onSignIn(username, password);
-    setUsername('');
-    setPassword('');
-  }
+
+    const formData = new URLSearchParams();
+    formData.append('state', 'oI7a4edGeu8kamVFhYkJqF2EWu2zFk9A');
+    formData.append('code', '111111');
+
+    const url = 'https://symmetrical-carnival-wj7r59qxprg2grj-3000.app.github.dev/auth/intra/signin';
+
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          // Add authentication headers if needed
+        },
+        body: formData.toString(),
+      });
+
+      if (response.ok) {
+        setMessage('Login successful');
+      } else {
+        const errorData = await response.json().catch(() => ({})); // Handle potential non-JSON responses
+        const errorMessage = errorData.message || 'An error occurred on the server.';
+        setMessage(`Error: ${errorMessage}`);
+      }
+    } catch (error) {
+      console.error('An error occurred:', error);
+      setMessage(`An error occurred while processing your request: ${error}`);
+    }
+  };
 
   return (
     <div style={formStyle}>
-      <h1 style={{fontFamily:'Arial Black', fontSize: '36px', color: 'white'}}>Pong Game</h1>
-      <img src="/assets/school_42.jpeg" alt='42 logo' style={{width: '150px', marginBottom: '12px'}} />
+      <h1 style={{ fontFamily: 'Arial Black', fontSize: '36px', color: 'white' }}>Pong Game</h1>
+      <img src="/assets/school_42.jpeg" alt='42 logo' style={{ width: '150px', marginBottom: '12px' }} />
       <form onSubmit={handleSubmit}>
-        <button style={{width: '175px'}} type="submit">Sign In with 42</button>
+        <button style={{ width: '175px' }} type="submit">Sign In with 42</button>
       </form>
+      {message && <p style={{ color: 'white' }}>{message}</p>}
     </div>
   );
 }
