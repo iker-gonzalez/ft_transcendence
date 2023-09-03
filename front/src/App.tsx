630725ce025d32d6e0d24a7d74495d15b9e88ad7
Iker González <84924263+iker-gonzalez@users.noreply.github.com>
2023-09-03 10:47:11 +0000

    Game Routing and Page created

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -4,6 +4,7 @@ import SetProfile from './pages/SetProfile';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import UserProfile from './pages/UserProfile';
+import Game from './pages/Game';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
             <Route path="/" element={<SignIn />} />
             <Route path="/set-profile" element={<SetProfile />} />
             <Route path="/profile" element={<UserProfile />} />
+            <Route path="/game" element={<Game />} />
           </Routes>
         </div>
         </Router>
