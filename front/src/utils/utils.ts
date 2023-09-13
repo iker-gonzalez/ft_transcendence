a91e1b04dec6e3cb172e4b5d7f6854d8f0e18030
Iker González <84924263+iker-gonzalez@users.noreply.github.com>
2023-09-13 07:26:18 +0000

    SignIn Redirect Uri established

--- front/src/utils/utils.ts
+++ front/src/utils/utils.ts
@@ -2,6 +2,7 @@
  * Depending if the current hostname is localhost or not (e.g. a GitHub Codespace), it returns the correct base url to use in API calls.
  * @returns the computed base url of the api
  */
+
 export function getBaseUrl(): string {
   const apiPort: string = process.env.REACT_APP_API_PORT || "3000";
   const clientPort: string = process.env.REACT_APP_CLIENT_PORT || "4200";
@@ -9,4 +10,13 @@ export function getBaseUrl(): string {
   return window.location.hostname === "localhost"
     ? `http://localhost:${apiPort}`
     : `https://${window.location.hostname.replace(apiPort, clientPort)}`;
+}
+
+export function getRedirectUri(): string {
+  const apiPort: string = process.env.REACT_APP_API_PORT || "3000";
+  const clientPort: string = process.env.REACT_APP_CLIENT_PORT || "4200";
+
+  return window.location.hostname === "localhost"
+    ? `http://localhost:${apiPort}/profile`
+    : `https://${window.location.hostname.replace(apiPort, clientPort)}/profile`;
 }
