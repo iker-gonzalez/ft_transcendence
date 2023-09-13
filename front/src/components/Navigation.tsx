7ed499984626160ff4ee3405c6a1386bb631985d
Iker González <84924263+iker-gonzalez@users.noreply.github.com>
2023-09-13 07:06:56 +0000

    refactor to styled components

--- front/src/components/Navigation.tsx
+++ front/src/components/Navigation.tsx
@@ -1,49 +1,55 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const navbarStyles = {
-  backgroundColor: '#393E46',
-  color: '#fff',
-  padding: '10px 0',
-  boxShadow: '0px 2px 5px rgba(0, 0, 0, 0.2)',
-};
+// Define styled components
+const NavbarContainer = styled.nav`
+  background-color: #393E46;
+  color: #fff;
+  padding: 10px 0;
+  box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.2);
+`;
 
-const navListStyles = {
-  listStyle: 'none',
-  padding: '0',
-  display: 'flex',
-  justifyContent: 'center', // Center-align menu items
-};
+const NavList = styled.ul`
+  list-style: none;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+`;
 
-const navItemStyles = {
-  margin: '0 20px',
-};
+const NavItem = styled.li`
+  margin: 0 20px;
+`;
 
-const navLinkStyles = {
-  textDecoration: 'none',
-  color: '#fff',
-  fontSize: '18px', // Increase font size
-  fontWeight: 'bold',
-  transition: 'color 0.3s ease-in-out', // Add color transition
-};
+const NavLink = styled.a`
+  text-decoration: none;
+  color: #fff;
+  font-size: 18px;
+  font-weight: bold;
+  transition: color 0.3s ease-in-out;
+
+  &:hover {
+    color: #00adb5; /* Change color on hover */
+  }
+`;
 
 const Navbar = () => {
   return (
-    <nav style={navbarStyles}>
-      <ul style={navListStyles}>
-        <li style={navItemStyles}>
-          <a href="/" style={navLinkStyles}>Home</a>
-        </li>
-        <li style={navItemStyles}>
-          <a href="/stats" style={navLinkStyles}>Stats</a>
-        </li>
-        <li style={navItemStyles}>
-          <a href="/profile" style={navLinkStyles}>Profile</a>
-        </li>
-        <li style={navItemStyles}>
-          <a href="/game" style={navLinkStyles}>Game</a>
-        </li>
-      </ul>
-    </nav>
+    <NavbarContainer>
+      <NavList>
+        <NavItem>
+          <NavLink href="/">Home</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="/stats">Stats</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="/profile">Profile</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="/game">Game</NavLink>
+        </NavItem>
+      </NavList>
+    </NavbarContainer>
   );
 };
 
