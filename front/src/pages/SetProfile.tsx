2b895a940a1558a901002004edd3d5f9e2b5a072
Iker González <84924263+iker-gonzalez@users.noreply.github.com>
2023-09-13 07:06:56 +0000

    refactor to styled components

--- front/src/pages/SetProfile.tsx
+++ front/src/pages/SetProfile.tsx
@@ -1,34 +1,85 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 
-const formStyle: React.CSSProperties = {
-    position: 'fixed',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-  };
-  
-  const buttonStyles = {
-    width: '175px',
-    height: '50px',
-    backgroundColor: 'gold',
-    color: 'black',
-    borderRadius: '5px',
-    border: 'none',
-    cursor: 'pointer',
-    fontSize: '16px',
-    fontWeight: 'bold',
-    boxShadow: '0px 3px 6px rgba(0, 0, 0, 0.1)',
-    marginTop: '30px'
-  };
+// Styled components
+const FormContainer = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Heading = styled.h2`
+  font-family: Arial;
+  font-size: 30px;
+  margin-bottom: 20px;
+`;
+
+const InputField = styled.div`
+  width: 150px;
+  margin-bottom: 20px;
+
+  input {
+    width: 100%;
+    padding: 10px;
+    font-size: 16px;
+    border: none;
+    border-radius: 5px;
+    box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.1);
+  }
+`;
+
+const AvatarInput = styled.input`
+  display: none;
+`;
+
+const AvatarLabel = styled.label`
+  width: 150px;
+  text-align: center;
+  cursor: pointer;
+  margin-bottom: 20px;
+  display: block;
+
+  img {
+    max-width: 200px;
+    max-height: 200px;
+    margin-top: 10px;
+  }
+`;
+
+const CheckboxLabel = styled.label`
+  display: flex;
+  align-items: center;
+  margin-top: 20px;
+  cursor: pointer;
+
+  input {
+    margin-right: 10px;
+  }
+`;
+
+const CreateButton = styled.button`
+  width: 175px;
+  height: 50px;
+  background-color: gold;
+  color: black;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 16px;
+  font-weight: bold;
+  box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.1);
+  margin-top: 30px;
+`;
 
 function SetProfile() {
   const [username, setUsername] = useState('');
   const [avatar, setAvatar] = useState<File | null>(null);
-  const [isTwoFactorEnabled, setIsTwoFactorEnabled] = useState(false); // State for enabling 2FA
+  const [isTwoFactorEnabled, setIsTwoFactorEnabled] = useState(false);
 
   const handleAvatarChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const selectedAvatar = e.target.files && e.target.files[0];
@@ -45,9 +96,9 @@ function SetProfile() {
   };
 
   return (
-    <div style={formStyle}>
-      <h2 style={{ fontFamily: 'Arial', fontSize: '30px'}}>Choose your username</h2>
-      <div style={{ width: '150px', marginBottom: '20px' }}>
+    <FormContainer>
+      <Heading>Choose your username</Heading>
+      <InputField>
         <label htmlFor="username"></label>
         <input
           type="text"
@@ -56,34 +107,32 @@ function SetProfile() {
           value={username}
           onChange={(e) => setUsername(e.target.value)}
         />
-      </div>
-      <h2 style={{ fontFamily: 'Arial', fontSize: '30px'}}>Choose your avatar</h2>
-      <input
-        type="file"
-        accept="image/*"
-        onChange={handleAvatarChange}
-      />
-      {avatar && (
-        <img
-          src={URL.createObjectURL(avatar)}
-          alt="Selected Avatar"
-          style={{ maxWidth: '200px', maxHeight: '200px', marginTop: '10px' }}
+      </InputField>
+      <Heading>Choose your avatar</Heading>
+      <AvatarLabel>
+        <AvatarInput
+          type="file"
+          accept="image/*"
+          onChange={handleAvatarChange}
         />
-      )}
-      <div style={{ marginTop: '20px' }}>
-        <label>
-          Enable Two-Factor Authentication
-          <input
-            type="checkbox"
-            checked={isTwoFactorEnabled}
-            onChange={handleTwoFactorChange}
+        {avatar && (
+          <img
+            src={URL.createObjectURL(avatar)}
+            alt="Selected Avatar"
           />
-        </label>
-      </div>
-      <button onClick={handleCreate} style={buttonStyles}>
-        Create
-      </button>
-    </div>
+        )}
+        {avatar ? 'Change Avatar' : 'Upload Avatar'}
+      </AvatarLabel>
+      <CheckboxLabel>
+        <input
+          type="checkbox"
+          checked={isTwoFactorEnabled}
+          onChange={handleTwoFactorChange}
+        />
+        Enable Two-Factor Authentication
+      </CheckboxLabel>
+      <CreateButton onClick={handleCreate}>Create</CreateButton>
+    </FormContainer>
   );
 }
 
