0f8fd7119f9cc1cc0b046225cff0314863e10e14
Nicolas Gasco <nicolas.gasco92@gmail.com>
2023-09-20 22:49:12 +0200

    Add UserProfileSettings component

---
+++ front/src/components/UserProfileSettings.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import styled from 'styled-components';
+import ContrastPanel from './UI/ContrastPanel';
+import MainButton from './UI/MainButton';
+import { primaryLightColor } from '../constants/color-tokens';
+
+const WrapperDiv = styled.div`
+  width: 650px;
+
+  .settings-item {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    &:not(:last-of-type) {
+      padding-bottom: 20px;
+      margin-bottom: 20px;
+      border-bottom: 1px ${primaryLightColor} solid;
+    }
+
+    p {
+      margin: 0;
+    }
+  }
+`;
+
+// TODO add TS interface for userData
+const UserProfileSettings: React.FC<{ userData: any }> = ({ userData }) => {
+  return (
+    <ContrastPanel>
+      <WrapperDiv>
+        <div className="settings-item">
+          <p className="title-3">OTP</p>
+          {userData.isTwoFactorAuthEnabled ? (
+            <p>On</p>
+          ) : (
+            <MainButton>Activate</MainButton>
+          )}
+        </div>
+        <div className="settings-item">
+          <p className="title-3">Email</p>
+          <MainButton>Change</MainButton>
+        </div>
+      </WrapperDiv>
+    </ContrastPanel>
+  );
+};
+
+export default UserProfileSettings;
