f9dd1dd30fc4d2de079f6844693c1346327e7a12
Nicolas Gasco <nicolas.gasco92@gmail.com>
2023-09-24 17:28:16 +0200

    Add button to abort username change

---
+++ front/src/components/UI/SecondaryButton.tsx
@@ -0,0 +1,30 @@
+import { styled } from 'styled-components';
+
+import {
+  darkBgColor,
+  primaryColor,
+  primaryLightColor,
+} from '../../constants/color-tokens';
+
+const Button = styled.button`
+  padding: 10px 25px;
+  background-color: transparent;
+  color: ${primaryLightColor};
+  border-radius: 5px;
+  border: 1px ${primaryColor} solid;
+  cursor: pointer;
+  font-size: 16px;
+  font-weight: bold;
+  box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.1);
+
+  &:disabled {
+    opacity: 0.65;
+    cursor: not-allowed;
+  }
+
+  &:hover {
+    background-color: ${darkBgColor};
+  }
+`;
+
+export default Button;
