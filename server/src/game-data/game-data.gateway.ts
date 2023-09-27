371971488a10c02b5ef97ba91b4272c0c707f12e
Nicolas Gasco <nicolas.gasco92@gmail.com>
2023-09-27 22:12:18 +0200

    Remove nestjs-asyncapi package

--- server/src/game-data/game-data.gateway.ts
+++ server/src/game-data/game-data.gateway.ts
@@ -6,7 +6,7 @@ import {
 } from '@nestjs/websockets';
 import { Server } from 'socket.io';
 import { GameDataService } from './game-data.service';
-import { AsyncApiPub, AsyncApiSub } from 'nestjs-asyncapi';
+// import { AsyncApiPub, AsyncApiSub } from 'nestjs-asyncapi';
 import { swaggerAsyncConstants } from '../../config/swagger-async.constants';
 import { StartGameDto } from './dto/start-game.dto';
 import { EmptyDto } from './dto/empty.dto';
@@ -21,46 +21,46 @@ export class GameDataGateway {
   @WebSocketServer()
   server: Server;
 
-  @AsyncApiPub({
-    channel: swaggerAsyncConstants.gameData.endpoints.startGame.channel,
-    description: swaggerAsyncConstants.gameData.endpoints.startGame.description,
-    message: {
-      payload: StartGameDto,
-    },
-  })
-  @AsyncApiSub({
-    channel:
-      swaggerAsyncConstants.gameData.endpoints.gameDataCreatedSessionId.channel,
-    description:
-      swaggerAsyncConstants.gameData.endpoints.gameDataCreatedSessionId
-        .description,
-    message: {
-      payload: EmptyDto,
-    },
-  })
+  // @AsyncApiPub({
+  //   channel: swaggerAsyncConstants.gameData.endpoints.startGame.channel,
+  //   description: swaggerAsyncConstants.gameData.endpoints.startGame.description,
+  //   message: {
+  //     payload: StartGameDto,
+  //   },
+  // })
+  // @AsyncApiSub({
+  //   channel:
+  //     swaggerAsyncConstants.gameData.endpoints.gameDataCreatedSessionId.channel,
+  //   description:
+  //     swaggerAsyncConstants.gameData.endpoints.gameDataCreatedSessionId
+  //       .description,
+  //   message: {
+  //     payload: EmptyDto,
+  //   },
+  // })
   @SubscribeMessage('startGame')
   onGameStart(@MessageBody() data: string): void {
     return this.gameDataService.onGameStart(this.server, data);
   }
 
-  @AsyncApiSub({
-    channel: swaggerAsyncConstants.gameData.endpoints.ready.channel,
-    description: swaggerAsyncConstants.gameData.endpoints.ready.description,
-    message: {
-      payload: ReadyPlayerDto,
-    },
-  })
-  @AsyncApiPub({
-    channel:
-      swaggerAsyncConstants.gameData.endpoints.allOpponentsReadySessionId
-        .channel,
-    description:
-      swaggerAsyncConstants.gameData.endpoints.allOpponentsReadySessionId
-        .description,
-    message: {
-      payload: EmptyDto,
-    },
-  })
+  // @AsyncApiSub({
+  //   channel: swaggerAsyncConstants.gameData.endpoints.ready.channel,
+  //   description: swaggerAsyncConstants.gameData.endpoints.ready.description,
+  //   message: {
+  //     payload: ReadyPlayerDto,
+  //   },
+  // })
+  // @AsyncApiPub({
+  //   channel:
+  //     swaggerAsyncConstants.gameData.endpoints.allOpponentsReadySessionId
+  //       .channel,
+  //   description:
+  //     swaggerAsyncConstants.gameData.endpoints.allOpponentsReadySessionId
+  //       .description,
+  //   message: {
+  //     payload: EmptyDto,
+  //   },
+  // })
   @SubscribeMessage('ready')
   onUser1Ready(@MessageBody() data: string): void {
     return this.gameDataService.onPlayerReady(this.server, data);
@@ -71,21 +71,21 @@ export class GameDataGateway {
     return this.gameDataService.uploadGameData(this.server, data);
   }
 
-  @AsyncApiSub({
-    channel: swaggerAsyncConstants.gameData.endpoints.downloaded.channel,
-    description:
-      swaggerAsyncConstants.gameData.endpoints.downloaded.description,
-    message: {
-      payload: ReadyPlayerDto,
-    },
-  })
-  @AsyncApiPub({
-    channel: swaggerAsyncConstants.gameData.endpoints.download.channel,
-    description: swaggerAsyncConstants.gameData.endpoints.download.description,
-    message: {
-      payload: EmptyDto, // TODO create DTO for this
-    },
-  })
+  // @AsyncApiSub({
+  //   channel: swaggerAsyncConstants.gameData.endpoints.downloaded.channel,
+  //   description:
+  //     swaggerAsyncConstants.gameData.endpoints.downloaded.description,
+  //   message: {
+  //     payload: ReadyPlayerDto,
+  //   },
+  // })
+  // @AsyncApiPub({
+  //   channel: swaggerAsyncConstants.gameData.endpoints.download.channel,
+  //   description: swaggerAsyncConstants.gameData.endpoints.download.description,
+  //   message: {
+  //     payload: EmptyDto, // TODO create DTO for this
+  //   },
+  // })
   @SubscribeMessage('download')
   onDownloadGameDataUser(@MessageBody() data: string): void {
     return this.gameDataService.downloadGameData(this.server, data);
