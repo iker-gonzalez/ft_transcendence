d462223204212454c04cabfe722ab46eefbec47b
Nicolas Gasco <nicolas.gasco92@gmail.com>
2023-10-03 22:18:02 +0200

    Fix LoadingPage logic on login

--- front/src/pages/LoadingPage.tsx
+++ front/src/pages/LoadingPage.tsx
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styled, { keyframes } from 'styled-components';
-import { useNavigate } from 'react-router-dom';
 import { primaryAccentColor } from '../constants/color-tokens';
 
 const spin = keyframes`
@@ -9,48 +8,34 @@ const spin = keyframes`
 `;
 
 const LoadingSpinner = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100vh;
 `;
 
 const Spinner = styled.div`
   border: 4px solid rgba(255, 255, 255, 0.3);
   border-radius: 50%;
   border-top: 4px solid ${primaryAccentColor}; /* Change the spinner color */
-  width: 50px;
-  height: 50px;
+  width: 60px;
+  height: 60px;
   animation: ${spin} 1s linear infinite;
 `;
 
-interface LoadingPageProps {
-  targetPath: string;
-}
-
-function LoadingPage({ targetPath }: LoadingPageProps) {
-  const [isLoading, setIsLoading] = useState(true);
-  const navigate = useNavigate();
-
-  // Simulate an asynchronous operation, like fetching data
-  useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false); // Set isLoading to false after a delay (simulating data loading)
-
-      // Navigate to the specified target path when loading is complete
-      navigate(targetPath);
-    }, 1000); // Adjust the delay as needed
-  }, [navigate, targetPath]);
-
+function LoadingPage(): JSX.Element {
   return (
     <div>
-      {
-        isLoading ? (
-          <LoadingSpinner>
-            <Spinner />
-          </LoadingSpinner>
-        ) : null /* No content to display */
-      }
+      <LoadingSpinner>
+        <Spinner />
+      </LoadingSpinner>
     </div>
   );
 }
