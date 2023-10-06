376fb07b39e4721f5e96a76182c1b8a4f70d0039
Nicolas Gasco <nicolas.gasco92@gmail.com>
2023-10-06 21:42:12 +0200

    Refactor Home component for clarity

--- front/src/pages/Home.tsx
+++ front/src/pages/Home.tsx
@@ -15,14 +15,25 @@ const PageWrapperDiv = styled.div`
   justify-content: center;
   align-items: center;
 
-  .signin-link {
-    color: ${primaryLightColor};
-    text-decoration: underline;
-    cursor: pointer;
+  .logo {
+    width: 150px;
+    margin-bottom: 12px;
+  }
+
+  .links-container {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+
+    .signin-link {
+      color: ${primaryLightColor};
+      text-decoration: underline;
+      cursor: pointer;
+    }
   }
 `;
 
-function SignIn() {
+const SignIn: React.FC = (): JSX.Element => {
   const {
     userData,
     setUserData,
@@ -34,23 +45,22 @@ function SignIn() {
     const token: string | undefined = Cookies.get('token');
 
     if (!token) {
+      // If there is no token, the user is not logged in
       setUserData(null);
-    }
-
-    if (token) {
+    } else {
       // User data is not set, but session is still active
-      if (!userData && token.length) {
+      if (!userData) {
         fetchUserData(token);
       }
     }
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-  const handleSignInClick = () => {
+  const handleSignIn = () => {
     // Redirect the user to the intranet URL
     window.location.href = `https://api.intra.42.fr/oauth/authorize?client_id=${process.env.REACT_APP_INTRA_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_INTRA_AUTH_REDIRECT_URI}&response_type=code`;
   };
 
-  const handleUserLogOut = () => {
+  const handleLogOut = () => {
     Cookies.remove('token');
     setUserData(null);
     window.location.reload();
@@ -64,26 +74,15 @@ function SignIn() {
         return (
           <>
             <h1 className="title-1">Pong Game</h1>
-            <img
-              src="/assets/school_42.png"
-              alt="42 logo"
-              style={{ width: '150px', marginBottom: '12px' }}
-            />
+            <img src="/assets/school_42.png" alt="" className="logo" />
             {userData ? (
               <>
                 <h2 className="title-2">Hello, {userData.username}!</h2>
-                <MainButton onClick={handleUserLogOut}>Log out</MainButton>
+                <MainButton onClick={handleLogOut}>Log out</MainButton>
               </>
             ) : (
-              <form
-                onSubmit={(e) => e.preventDefault()} // Prevent form submission
-                style={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  gap: '20px',
-                }}
-              >
-                <MainButton type="button" onClick={handleSignInClick}>
+              <div className="links-container">
+                <MainButton type="button" onClick={handleSignIn}>
                   Sign In with 42
                 </MainButton>
                 <Link
@@ -98,13 +97,13 @@ function SignIn() {
                 >
                   Sign in with test user 2
                 </Link>
-              </form>
+              </div>
             )}
           </>
         );
       })()}
     </PageWrapperDiv>
   );
-}
+};
 
 export default SignIn;
