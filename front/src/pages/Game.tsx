0876c3dbac76c4564a93e177859fad2e1f6da191
Nicolas Gasco <nicolas.gasco92@gmail.com>
2023-10-07 22:31:45 +0200

    Fix error when reloading one of the game pages

--- front/src/pages/Game.tsx
+++ front/src/pages/Game.tsx
@@ -1,22 +1,35 @@
-import React, { useState } from 'react';
-import { Outlet, useOutletContext } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Outlet, useNavigate, useOutletContext } from 'react-router-dom';
 import SessionData from '../interfaces/game-session-data.interface';
+import { useUserData } from '../context/UserDataContext';
+import UserData from '../interfaces/user-data.interface';
+import UserDataContextData from '../interfaces/user-data-context-data.interface';
 
 type GameContextType = {
   sessionDataState: [
     sessionData: SessionData,
     setSessionData: React.Dispatch<React.SetStateAction<SessionData>>,
   ];
+  userData: UserData;
 };
 
 const Game: React.FC = (): JSX.Element => {
   const [sessionData, setSessionData] = useState<SessionData>();
+  const { userData }: UserDataContextData = useUserData();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!userData) {
+      navigate('/');
+    }
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <main>
       <Outlet
         context={{
           sessionDataState: [sessionData, setSessionData],
+          userData,
         }}
       />
     </main>
