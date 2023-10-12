0a98d820a7d7289ad1eb5ebc244e0fed9a6f83d3
Nicolas Gasco <nicolas.gasco92@gmail.com>
2023-10-12 19:44:27 +0200

    Add JwtGuard to POST game/data endpoint

--- server/src/game/game-data.controller.ts
+++ server/src/game/game-data.controller.ts
@@ -1,9 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  ApiCreatedResponse,
+  ApiOperation,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 import { swaggerConstants } from '../../config/swagger.constants';
 import { GameDataService } from './game-data.service';
 import { NewGameDataSetBodyDto } from './dto/new-game-data-set-body.dto';
 import { NewGameDataResponseDto } from './dto/new-game-data-response.dto';
+import { JwtGuard } from '../auth/guard/jwt.guard';
 
 @ApiTags('Game data')
 @Controller('game/data')
@@ -18,6 +24,10 @@ export class GameDataController {
     description: swaggerConstants.game.data.new.created.description,
     type: NewGameDataResponseDto,
   })
+  @ApiUnauthorizedResponse({
+    description: swaggerConstants.game.data.new.unauthorized.description,
+  })
+  @UseGuards(JwtGuard)
   createNewSession(
     @Body() newGameDataSetDto: NewGameDataSetBodyDto,
   ): Promise<any> {
