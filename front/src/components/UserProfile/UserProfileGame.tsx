3588cc711a310a19dbf706578458950705983bc1
Nicolas Gasco <nicolas.gasco92@gmail.com>
2023-10-14 15:14:32 +0200

    Show match history in User profile

---
+++ front/src/components/UserProfile/UserProfileGame.tsx
@@ -0,0 +1,147 @@
+import React, { useEffect } from 'react';
+import UserData from '../../interfaces/user-data.interface';
+import styled from 'styled-components';
+import ContrastPanel from '../UI/ContrastPanel';
+import { useUserGames } from '../../context/UserDataContext';
+import RoundImg from '../UI/RoundImage';
+import { primaryLightColor } from '../../constants/color-tokens';
+import UserGameData from '../../interfaces/user-game-data.interface';
+import UserGameDataPlayer from '../../interfaces/user-game-data-player.interface';
+
+const WrapperDiv = styled.div`
+  width: 650px;
+
+  .session-box {
+    padding: 24px 0;
+
+    &:not(:last-of-type) {
+      border-bottom: 1px ${primaryLightColor} solid;
+    }
+  }
+
+  .title-box {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 24px;
+
+    margin-bottom: 24px;
+  }
+
+  .players-box {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 36px;
+
+    .player-box {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      gap: 16px;
+
+      .score-box {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 8px;
+
+        &.right {
+          flex-direction: row-reverse;
+        }
+
+        .avatar {
+          width: 75px;
+        }
+      }
+    }
+  }
+`;
+
+const UserProfileGame: React.FC<{ userData: UserData }> = ({
+  userData,
+}): JSX.Element => {
+  const { userGames, isFetchingGames, fetchGamesList, isErrorFetchingGames } =
+    useUserGames();
+
+  useEffect(() => {
+    fetchGamesList(userData.intraId);
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return (
+    <ContrastPanel>
+      <WrapperDiv>
+        <h2 className="title-2 mb-16">Game history</h2>
+        {(() => {
+          if (isErrorFetchingGames) {
+            return (
+              <p>
+                There was an error fetching your game history. Try reloading the
+                page.
+              </p>
+            );
+          }
+
+          if (isFetchingGames) {
+            return <p>Loading...</p>;
+          }
+
+          return (
+            <div>
+              {userGames.map((game: UserGameData, index) => {
+                const formattedDate = new Date(
+                  game.startedAt,
+                ).toLocaleDateString('en-us', {
+                  year: 'numeric',
+                  month: 'long',
+                  day: 'numeric',
+                });
+                const gameDuration = new Date(game.elapsedTime).getSeconds();
+
+                return (
+                  <div key={game.sessionId} className="session-box">
+                    <div className="title-box">
+                      <h3 className="title-3">Match #{index + 1}</h3>
+                      <p className="small">
+                        Played on {formattedDate} for {gameDuration} seconds.
+                      </p>
+                    </div>
+                    <div className="players-box">
+                      {game.players
+                        .sort((a: UserGameDataPlayer) => {
+                          return a.intraId === userData.intraId ? -1 : 1;
+                        })
+                        .map((player: UserGameDataPlayer, indexPlayer) => {
+                          return (
+                            <div className="player-box">
+                              <p className="title-3">
+                                {player.username} {player.isWinner && '🏆'}
+                              </p>
+                              <div
+                                className={`score-box ${
+                                  indexPlayer % 2 !== 0 ? 'right' : 'left'
+                                }`}
+                              >
+                                <p className="title-1">{player.score}</p>
+                                <RoundImg
+                                  src={player.avatar}
+                                  alt=""
+                                  className="avatar"
+                                />
+                              </div>
+                            </div>
+                          );
+                        })}
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })()}
+      </WrapperDiv>
+    </ContrastPanel>
+  );
+};
+
+export default UserProfileGame;
