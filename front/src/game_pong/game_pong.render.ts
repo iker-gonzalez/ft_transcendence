9a622a043bf66852cfefa8b98efe1a3d37197e46
Diego Gerwig <dgerwig-@student.42urduliz.com>
2023-10-28 13:01:20 +0200

    Copy score logic in matchUser2

--- front/src/game_pong/game_pong.render.ts
+++ front/src/game_pong/game_pong.render.ts
@@ -327,9 +327,9 @@ export function matchUser1(
   }
 
   // Check if ball pass the goal line & increase user score
-  // If a goal is scored, the ball is reset
+  // If a goal is scored, the ball & paddle are reset to initial values
   if (ballData.x + ballData.radius < 15 && !ballData.reset) {
-    user2.score++;
+    //user2.score++;
     let { newBallData, newUserData1, newUserData2 } = resetBall(
       canvas,
       ballData,
@@ -412,7 +412,71 @@ export function matchUser2(
     canvas.height - thickness - user2.height - ballData.radius * slit
   ) {
     user2.y = canvas.height - thickness - user2.height - ballData.radius * slit;
-  } 
+  }
+
+  // Check if ball pass the goal line & increase user score
+  // If a goal is scored, the ball & paddle are reset to initial values
+  if (ballData.x + ballData.radius < 15 && !ballData.reset) {
+    //user2.score++;
+    let { newBallData, newUserData1, newUserData2 } = resetBall(
+      canvas,
+      ballData,
+      user1,
+      user2,
+      userSpeedInput,
+    );
+    ballData = newBallData;
+    user1 = newUserData1;
+    user2 = newUserData2;
+  } else if (
+    ballData.x + ballData.radius > canvas.width - 15 &&
+    !ballData.reset
+  ) {
+    user1.score++;
+    // sounds.userScore.play().catch(function (error: any) {
+    //   // console.log("Chrome cannot play sound without user interaction first");
+    // });
+    let { newBallData, newUserData1, newUserData2 } = resetBall(
+      canvas,
+      ballData,
+      user1,
+      user2,
+      userSpeedInput,
+    );
+    ballData = newBallData;
+    user1 = newUserData1;
+    user2 = newUserData2;
+  }
+
+  // Detect if the ball is in the court of user1 or user2
+  let player: IUserData =
+    ballData.x + ballData.radius < canvas.width / 2 ? user1 : user2;
+
+  // Detect if the ball hits the paddle & bounce the ball
+  if (checkCollision(ballData, player)) {
+    // sounds.hit.play().catch(function (error: any) {
+    //   // console.log("Chrome cannot play sound without user interaction first");
+    // });
+
+    // Detect the point where the ball hits in the paddle
+    let collidePoint = ballData.y - (player.y + player.height / 2);
+    collidePoint = collidePoint / (player.height / 2);
+
+    let angleRad = (Math.PI / 4) * collidePoint;
+
+    // Get direction to bounce the ball
+    let direction = ballData.x + ballData.radius < canvas.width / 2 ? 1 : -1;
+    ballData.moveX = direction * ballData.speed * Math.cos(angleRad);
+    ballData.moveY = ballData.speed * Math.sin(angleRad);
+
+    // Modify values to make it more difficult
+    ballData.speed += 0.1;
+    user1.height -= 2;
+    user2.height -= 2;
+
+    // Sparks effect when the ball hits the paddle
+    sparks(canvas, ballData.x, ballData.y, ballData.radius, RenderColor.Yellow);
+  }
 }
 
 export function onGameEnd(
