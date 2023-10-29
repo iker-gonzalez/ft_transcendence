5ba8afd69097e06ca4f0d441727c31d43b4043ac
zcanales <zaloacanales@github.com>
2023-10-29 17:44:16 +0100

    Refactor, argumentos de los métodos son el id del usuario, no el intraid

--- server/src/chat/chat.service.ts
+++ server/src/chat/chat.service.ts
@@ -9,27 +9,8 @@ import { UserService } from 'src/user/user.service';
 export class ChatService {
   constructor(private readonly prisma: PrismaService) {}
   
-  async getUser(clientId: string) 
-  {
-    let userData: User; 
-    try {
-      userData = await this.prisma.user.findUniqueOrThrow({
-        where: { id: clientId },
-        include : { sentMessages : true},
-      });  }
-      catch (error) {
-           console.log( "erorr");
-           return;
-        };
-      return {
-        created: 1, 
-        data : {
-          id: userData.id,
-        }
-      }  
-  }
-
-  async getDMUsers(userId1: string, userId2: string):
+  // Sens DM between two user, sorted by time created.
+  async getDMBetweenUsers(userId1: string, userId2: string):
     Promise<any[]>
   {
     const conversationMessages = await this.prisma.directMessage.findMany({
@@ -51,6 +32,37 @@ export class ChatService {
     });
     return conversationMessages;
   }
+
+    // Sens DM between two user, sorted by time created.
+    async getAllUserDMWith(userId: string):
+    Promise<any[]>
+  {
+    const usersWithConversations = await this.prisma.directMessage.findMany({
+      where: {
+        OR: [
+          { senderId: userId },
+          { receiverId: userId },
+        ],
+      },
+      distinct: ['senderId', 'receiverId'], // Selecciona valores únicos de senderId y receiverId
+      select: {
+        senderId: true,
+        receiverId: true,
+      },
+    });
+    
+    // Combina los valores de senderId y receiverId en una sola lista
+    const allUsers = [
+      ...usersWithConversations.map((message) => message.senderId),
+      ...usersWithConversations.map((message) => message.receiverId),
+    ];
+    
+    // Elimina duplicados y el propio usuario actual
+    const uniqueUsers = Array.from(new Set(allUsers)).filter((user) => user !== userId);
+    
+    return uniqueUsers;
+  }
+
 
   async getMessagesByUser(
     intraId: string,
@@ -90,22 +102,22 @@ export class ChatService {
 
 
   async addMessageToUser(
-    senderIntraId: string,
-    receiverIntraId: string,
+    userSenderId: string,
+    userReceiverId: string,
     content: string
   ): Promise<void> 
   {
-    const idSender = this.findUserIdByIntraId(parseInt(senderIntraId, 10));
-    const idReceiver = this.findUserIdByIntraId(parseInt(receiverIntraId, 10));
+ //   const idSender = this.findUserIdByIntraId(parseInt(senderIntraId, 10));
+ //   const idReceiver = this.findUserIdByIntraId(parseInt(receiverIntraId, 10));
     try {
-      const userSenderId = await idSender;
-      console.log("userSenderId");
-      console.log(userSenderId);
+ //    const userSenderId = await idSender;
+ //    console.log("userSenderId");
+ //    console.log(userSenderId);
 
-      const userReceiverId = await idReceiver;
-      console.log("userReceiverId");
-      console.log(userReceiverId);
- 
+ //    const userReceiverId = await idReceiver;
+ //    console.log("userReceiverId");
+ //    console.log(userReceiverId);
+ //
       const existingMessage = await this.prisma.directMessage.findUnique({
         where: { id: userSenderId },
       });  
@@ -113,7 +125,6 @@ export class ChatService {
       if (!existingMessage || existingMessage) {
 
         console.log("exisingMessage");
-
         // El registro no se encontró, así que créalo
         const newMessage = await this.prisma.directMessage.create({
           data: {
@@ -122,19 +133,6 @@ export class ChatService {
             content: content,
           },
         });
-        // El nuevo mensaje ha sido creado
-      } else {
-        //por aqui no esntra, lo tengo para un futuro si es necesario
-        // El registro ya existe, simplemente actualízalo
-        const updatedMessage = await this.prisma.directMessage.update({
-          where: { id: userSenderId},
-          data: {
-            senderId: userSenderId,
-            receiverId: userReceiverId,
-            content: content,
-          },
-        });
-        // El mensaje existente ha sido actualizado
       }
   }
     catch(e)
