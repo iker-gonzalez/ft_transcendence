078c1ede2f11b4baeba0b0e86cf75beb61a016e4
zcanales <zaloacanales@github.com>
2023-10-30 20:33:33 +0100

    Endpoints and documentation updated

--- server/src/chat/chat.controller.ts
+++ server/src/chat/chat.controller.ts
@@ -31,12 +31,9 @@ export class ChatController {
     @Get(':userId1/:userId2') // Define la ruta para los parámetros userId1 y userId2
     @ApiParam({ name: 'userId1' }) 
     @ApiParam({ name: 'userId2' }) 
-    @ApiBadRequestResponse({
-        description: swaggerConstants.friends.delete.bad.description,
-      })
-      @ApiUnauthorizedResponse({
-        description: swaggerConstants.friends.delete.unauthorized.description,
-      })
+    @ApiOperation({
+      summary: swaggerConstants.chat.data.summary,
+    })
     async getDMBetweenUsers(
       @Param('userId1') userId1: string,
       @Param('userId2') userId2: string
@@ -47,6 +44,9 @@ export class ChatController {
 
     @Get('all/:userId') // Define una nueva ruta para el método getAllUserDMWith
     @ApiParam({ name: 'userId' }) 
+    @ApiOperation({
+      summary: swaggerConstants.chat.all.summary,
+    })
     async getAllUserDMWith(@Param('userId') userId: string): Promise<any[]> {
         return this.chatService.getAllUserDMWith(userId);
     }
