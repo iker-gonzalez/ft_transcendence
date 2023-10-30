36b8f82ae508f7030345ce4f82cb535270b60a2d
Nicolas Gasco <nicolas.gasco92@gmail.com>
2023-10-30 23:07:21 +0100

    Hide menu items that should not be visible by unlogged users

--- front/src/components/Navigation.tsx
+++ front/src/components/Navigation.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import {
   darkBgColor,
@@ -6,8 +6,8 @@ import {
   primaryAccentColor,
 } from '../constants/color-tokens';
 import { Link } from 'react-router-dom';
+import { useUserData } from '../context/UserDataContext';
 
-// Define styled components
 const NavbarContainer = styled.nav`
   background-color: ${darkBgColor};
   color: ${primaryLightColor};
@@ -15,48 +15,76 @@ const NavbarContainer = styled.nav`
   box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.2);
 `;
 
-const NavList = styled.ul`
+const NavList = styled.nav`
   list-style: none;
   padding: 0;
   display: flex;
   justify-content: center;
-`;
+  gap: 30px;
 
-const NavItem = styled.li`
-  margin: 0 20px;
-`;
+  .link {
+    text-decoration: none;
+    color: ${primaryLightColor};
+    font-size: 18px;
+    font-weight: bold;
+    transition: color 0.3s ease-in-out;
 
-const NavLink = styled(Link)`
-  text-decoration: none;
-  color: ${primaryLightColor};
-  font-size: 18px;
-  font-weight: bold;
-  transition: color 0.3s ease-in-out;
+    &.disabled {
+      cursor: not-allowed;
+      opacity: 0.5;
+    }
 
-  &:hover {
-    color: ${primaryAccentColor}; /* Change color on hover */
+    &:hover:not(.disabled) {
+      color: ${primaryAccentColor}; /* Change color on hover */
+    }
   }
 `;
 
 const Navbar = (): JSX.Element => {
+  const { userData } = useUserData();
+  const [isLogged, setIsLogged] = React.useState<boolean>(!!userData);
+
+  useEffect(() => {
+    setIsLogged(!!userData);
+  }, [userData]);
+
+  const disableLink = (e: any) => {
+    if (!isLogged) {
+      e.preventDefault();
+      return;
+    }
+  };
+
   return (
     <NavbarContainer>
       <NavList>
-        <NavItem>
-          <NavLink to="/">Home</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink to="/profile">Profile</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink to="/game">Play</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink to="/stats">Stats</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink to="/leaderboard">Leaderboard</NavLink>
-        </NavItem>
+        <Link to="/" className="link">
+          Home
+        </Link>
+        <Link
+          to="/profile"
+          onClick={disableLink}
+          className={`${!isLogged && 'disabled'} link`}
+        >
+          Profile
+        </Link>
+        <Link
+          to="/game"
+          onClick={disableLink}
+          className={`${!isLogged && 'disabled'} link`}
+        >
+          Play
+        </Link>
+        <Link
+          to="/stats"
+          onClick={disableLink}
+          className={`${!isLogged && 'disabled'} link`}
+        >
+          Stats
+        </Link>
+        <Link to="/leaderboard" className="link">
+          Leaderboard
+        </Link>
       </NavList>
     </NavbarContainer>
   );
