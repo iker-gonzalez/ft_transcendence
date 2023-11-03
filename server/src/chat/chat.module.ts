63b807bb1e6e8cc6b0f74fa76cb9531b6557f7ab
Nicolas Gasco <nicolas.gasco92@gmail.com>
2023-11-03 20:19:56 +0100

    Controller added, estar api docu

--- server/src/chat/chat.module.ts
+++ server/src/chat/chat.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ChatGateway } from './chat.gateway';
 import { ChatService } from './chat.service';
+import { ChatController } from './chat.controller';
 import { UserService } from '../user/user.service';
 import { UserModule } from '../user/user.module';
 
 @Module({
+  controllers : [ChatController],
   imports: [UserModule],
   providers: [ChatGateway, ChatService, UserService],
 })
