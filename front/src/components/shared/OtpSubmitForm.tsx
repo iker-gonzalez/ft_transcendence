d3c2852fb976d25667b8c2d145cbcd64ce8968b7
Nicolas Gasco <nicolas.gasco92@gmail.com>
2023-11-04 10:46:00 +0100

    Use shared component for OTP input form

---
+++ front/src/components/shared/OtpSubmitForm.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { OTP_LENGTH } from '../../constants/shared';
+import MainButton from '../UI/MainButton';
+import MainInput from '../UI/MainInput';
+import styled from 'styled-components';
+
+type OtpSubmitFormProps = {
+  otpValue: string;
+  setOtpValue: React.Dispatch<React.SetStateAction<string>>;
+  handleActivateWithOTP: () => void;
+};
+
+const StyledForm = styled.form`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+`;
+
+const OtpSubmitForm: React.FC<OtpSubmitFormProps> = ({
+  otpValue,
+  setOtpValue,
+  handleActivateWithOTP,
+}): JSX.Element => {
+  return (
+    <StyledForm
+      onSubmit={(e) => {
+        e.preventDefault();
+
+        if (otpValue.length !== OTP_LENGTH) return;
+
+        handleActivateWithOTP();
+      }}
+    >
+      <MainInput
+        type="text"
+        placeholder="Enter OTP"
+        maxLength={OTP_LENGTH}
+        value={otpValue}
+        onChange={(e) => {
+          const value = e.target.value;
+          if (value.match(/^[0-9]*$/)) {
+            setOtpValue(e.target.value);
+          }
+        }} // Update state on input change
+      />
+      <MainButton type="submit">Sign In</MainButton>
+    </StyledForm>
+  );
+};
+
+export default OtpSubmitForm;
