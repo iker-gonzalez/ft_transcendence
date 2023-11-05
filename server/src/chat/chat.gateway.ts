d15019413f0e6c3ddb272f2efa2ba11c60008fe9
zcanales <zcanales0001@gmail.com>
2023-11-05 11:58:55 +0100

    Direct menssage con username y avatar, borrador de char room y cambbio de parametro de entradas siempre se pide el userId y no el intraId

--- server/src/chat/chat.gateway.ts
+++ server/src/chat/chat.gateway.ts
@@ -1,9 +1,10 @@
 // chat.gateway.js
-import  { WebSocketGateway, WebSocketServer, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage } from '@nestjs/websockets';
+import  { WebSocketGateway, WebSocketServer, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, ConnectedSocket, MessageBody } from '@nestjs/websockets';
 import  { Server } from 'socket.io';
-import { ChatService } from './chat.service';
+import { ChatDMService } from './service/chatDM.service';
+import { ChatChannelService } from './service/chatChannel.service';
 import { UserService } from '../user/user.service';
-import { Socket } from 'socket.io-client';
+import { Socket } from 'socket.io';
 
 
 // Create a Map to store user sockets
@@ -13,7 +14,8 @@ const userSockets = new Map();
   namespace: 'chat'
 })
 export class ChatGateway implements OnGatewayConnection {
-  constructor(private readonly chatService: ChatService,
+  constructor(private readonly chatDMservice: ChatDMService,
+    private readonly chatChannelservice: ChatChannelService,
     private readonly userService: UserService) {}
 
   @WebSocketServer() 
@@ -29,35 +31,69 @@ export class ChatGateway implements OnGatewayConnection {
   async handlePrivateMessage(client, payload) {
     console.log("receiverId");
     console.log(payload.receiverId);
-    console.log("senderId");
-    console.log(payload.senderId);
-    console.log(parseInt(payload.senderId, 10));
-    
-    console.log("contents"); 
-    console.log(payload.content);  
-    
     try {
       // Prueba para el get de lo DM
-      const idSernder = await this.chatService.findUserIdByIntraId(parseInt(payload.senderId, 10));
-      console.log(idSernder);
-      const idReceiver = await this.chatService.findUserIdByIntraId(parseInt(payload.receiverId, 10));
-
-      const addMessageStatus =  await this.chatService.addMessageToUser(idSernder, idReceiver, payload.content);
+      const addMessageStatus =  await this.chatDMservice.addMessageToUser(payload.senderId, payload.receiverId, payload.content);
     
       // Pruebas de getters
-      const allMD2 = await this.chatService.getDMBetweenUsers(idSernder, idReceiver);
-      console.log("allMD2", allMD2);
+      //const allMD2 = await this.chatDMservice.getDMBetweenUsers(idSernder, idReceiver);
+      //console.log("allMD2", allMD2);
 
-      const usersDM = await this.chatService.getAllUserDMWith(idSernder);
-      console.log("userDM", usersDM);
+      //const usersDM = await this.chatDMservice.getAllUserDMWith(idSernder);
+      //console.log("userDM", usersDM);
+
+      //Pruebas Post
+    //  await this.chatChannelservice.createChannel(idSernder, "CanalUno", "public");
+    //  await this.chatChannelservice.addUserToChannel
+    //  ("1cc83703-a2ed-4ec2-b021-c5db82bb3d94", "CanalUno");
+    //
+    //  await this.chatChannelservice.addUserToChannel
+    //  ("31f0dd9b-c8fa-4df3-a07c-6bd5e40c5643", "CanalUno");
 
 
     } catch (error) {
       console.error("Error:", error);
     }
 
-    // Emit signal to update the sender chat frontend
-    this.server.emit(`privateMessageReceived/${payload.intraId}`,
+    // Emit signal to update the sender?receiver? chat frontend
+    this.server.emit(`privateMessageReceived/${payload.receiverId}`,
                       JSON.stringify(payload))
 }
+
+@SubscribeMessage('joinRoom')
+handleJoinRoom(@ConnectedSocket() client: Socket, @MessageBody() roomName: string) {
+  // Unir al cliente a la sala
+  console.log("lkfjdfs");
+  client.join(roomName);
+}
+
+
+@SubscribeMessage('channelMessage')
+async handleChannelMessage(client, payload) {
+  console.log(payload);
+//
+//console.log("channelRoorm");
+// console.log(payload.channelRoorm);
+// console.log("senderId");
+// console.log(payload.senderId);
+// console.log("contents"); 
+// console.log(payload.content);  
+// 
+// try {
+//   // Prueba para el get de lo chanel
+//  await this.chatChannelservice.addChannelMessageToUser(payload.chatRoom, payload.senderId, payload.content);
+// 
+// } catch (error) {
+//   console.error("Error:", error);
+// }
+
+  // Emit signal to update the sender?receiver? chat frontend
+//const mess = payload.mes;
+  // Emitir el mensaje a todos los clientes en la sala
+  //client.to(payload.roomName).emit('newMessage', { mess, senderId: client.id });
+}
+
+
+
+
 }
---
+++ server/src/chat/dto/all-users-DM-with.dto.ts
@@ -0,0 +1,22 @@
+import { IsUUID, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { swaggerConstants } from '../../../config/swagger.constants';
+
+
+export class AllUsersDMWithDTO {
+    @ApiProperty({
+        description: swaggerConstants.dto.allusersDMWithDTO.data.description,
+        example: swaggerConstants.dto.allusersDMWithDTO.data.example,
+      })
+
+    @IsUUID()
+    id: string;
+
+    @IsString()
+    avatar: string;
+
+    @IsString()
+    username: string;
+
+}
+  
