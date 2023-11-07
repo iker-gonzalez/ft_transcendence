05b0f11b602ed2c20d905f6b2866e004e625c9ae
Nicolas Gasco <nicolas.gasco92@gmail.com>
2023-11-07 19:21:34 +0100

    Improve styles of Home for Mobile breakpoints

--- front/src/components/UI/CenteredLayout.tsx
+++ front/src/components/UI/CenteredLayout.tsx
@@ -1,8 +1,14 @@
 import { styled } from 'styled-components';
+import { sm } from '../../constants/styles';
 
 const CenteredLayout = styled.div`
-  padding: 60px 100px;
-  padding-top: 130px;
+  padding: 30px 15px;
+  padding-top: 100px;
+
+  @media (width > ${sm}) {
+    padding: 60px 100px;
+    padding-top: 130px;
+  }
 
   display: flex;
   flex-direction: column;
