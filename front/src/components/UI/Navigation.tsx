1fab9ad9e398184d6216d55e455caadbed617cb2
Nicolas Gasco <nicolas.gasco92@gmail.com>
2023-11-07 19:10:17 +0100

    Fix styles of mobile navigation component

--- front/src/components/UI/Navigation.tsx
+++ front/src/components/UI/Navigation.tsx
@@ -20,9 +20,6 @@ const NavBarContainerMobile = styled.div`
   left: 0;
   bottom: 0;
   width: 100vw;
-  min-height: 100vh;
-  height: 100%;
-
   z-index: 100;
 
   .header {
@@ -143,6 +140,23 @@ const Navbar = (): JSX.Element => {
     }, ANIMATION_DURATION * 1000);
   };
 
+  const onClosingSidebar = () => {
+    if (menuButtonRef.current && !isLottiePlay) {
+      setIsSidebarOpen((prevState) => {
+        if (prevState === true) startClosingAnimation();
+
+        return !prevState;
+      });
+
+      const midFrame = MENU_BUTTON_FRAMES / 2;
+      if (!isSidebarOpen) {
+        menuButtonRef.current.playSegments([0, midFrame]);
+      } else {
+        menuButtonRef.current.playSegments([midFrame, MENU_BUTTON_FRAMES]);
+      }
+    }
+  };
+
   return (
     <>
       <NavBarContainerMobile>
@@ -157,25 +171,7 @@ const Navbar = (): JSX.Element => {
             onComplete={() => {
               setIsLottiePlay(false);
             }}
-            onClick={() => {
-              if (menuButtonRef.current && !isLottiePlay) {
-                setIsSidebarOpen((prevState) => {
-                  if (prevState === true) startClosingAnimation();
-
-                  return !prevState;
-                });
-
-                const midFrame = MENU_BUTTON_FRAMES / 2;
-                if (!isSidebarOpen) {
-                  menuButtonRef.current.playSegments([0, midFrame]);
-                } else {
-                  menuButtonRef.current.playSegments([
-                    midFrame,
-                    MENU_BUTTON_FRAMES,
-                  ]);
-                }
-              }
-            }}
+            onClick={onClosingSidebar}
             lottieRef={menuButtonRef}
             className="menu-icon"
           />
@@ -190,10 +186,7 @@ const Navbar = (): JSX.Element => {
             >
               <NavigationLinks
                 className="nav-list"
-                onClickLink={() => {
-                  setIsSidebarOpen(false);
-                  startClosingAnimation();
-                }}
+                onClickLink={onClosingSidebar}
               />
             </div>
             <div className="sidebar-bg animate__animated animate__fadeIn"></div>
