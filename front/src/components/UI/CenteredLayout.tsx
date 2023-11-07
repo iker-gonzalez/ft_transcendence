d16de937a517e15ecb435f49ff434faafb286057
Nicolas Gasco <nicolas.gasco92@gmail.com>
2023-11-07 20:12:49 +0100

    Make Leaderboard page responsive

--- front/src/components/UI/CenteredLayout.tsx
+++ front/src/components/UI/CenteredLayout.tsx
@@ -4,6 +4,7 @@ import { sm } from '../../constants/styles';
 const CenteredLayout = styled.div`
   padding: 30px 15px;
   padding-top: 100px;
+  max-width: 100vw;
 
   @media (width > ${sm}) {
     padding: 60px 100px;
