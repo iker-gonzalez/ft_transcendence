59d1e3abf59579e6679826f09931b375bbe84b22
Iker González Peña <ikergpe@gmail.com>
2023-11-09 15:46:10 +0100

    List of friends retrieved in order to open DM in Chat

--- front/src/components/Chat/StartChatPopup.tsx
+++ front/src/components/Chat/StartChatPopup.tsx
@@ -1,40 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import User from '../../interfaces/chat-user.interface';
-import { useUserData } from '../../context/UserDataContext';
-import { fetchAuthorized, getBaseUrl } from '../../utils/utils';
-import Cookies from 'js-cookie';
+import React from 'react';
+import { useUserFriends } from '../../context/UserDataContext';
 
 const StartChatPopup: React.FC<{ onClose: () => void }> = ({ onClose }) => {
     
-    const [friends, setFriends] = useState<User[]>([]);
-
-    const { userData } = useUserData();
-
-    useEffect(() => {
-      // Fetch all users that the signed in user has chatted with privately
-      if (userData) {
-        fetchAuthorized(`${getBaseUrl()}/friends/${userData?.intraId}`, {
-        headers: {
-          Authorization: `Bearer ${Cookies.get('token')}`,
-        },
-      })
-      .then(response => response.json())
-      .then((data: User[]) => { //Could we return an armonized object of users?
-      
-      const users = data.map(item => {
-        return {
-          id: item.id,
-          avatar: item.avatar,
-          username: item.username
-        }
-      });
-        setFriends(users);
-      });
-    }}, [userData]); // Added dependency array
+    const { userFriends } = useUserFriends();
 
     return (
       <div style={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', backgroundColor: 'white', padding: '20px', zIndex: 100 }}>
         {/* Your popup content goes here */}
+        {userFriends.map((friend, index) => (
+          <p key={index}>{friend.username}</p>
+        ))}
         <button onClick={onClose}>Close</button>
       </div>
     );
