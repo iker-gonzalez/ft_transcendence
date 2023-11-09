4d01410e0cb1c001f5334f1d0383314adf7f0955
Iker González Peña <ikergpe@gmail.com>
2023-11-09 17:54:08 +0100

    List of friends added to Chat Modal

--- front/src/components/Chat/StartChatPopup.tsx
+++ front/src/components/Chat/StartChatPopup.tsx
@@ -4,6 +4,7 @@ import { useUserFriends } from '../../context/UserDataContext';
 const StartChatPopup: React.FC<{ onClose: () => void }> = ({ onClose }) => {
     
     const { userFriends } = useUserFriends();
+    console.log('friends:', userFriends);
 
     return (
       <div style={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', backgroundColor: 'white', padding: '20px', zIndex: 100 }}>
