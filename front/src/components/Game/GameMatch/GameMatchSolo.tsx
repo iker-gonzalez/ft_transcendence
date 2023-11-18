1cb0a4c126277abb18b6a23f51ea6a6563689b18
Diego Gerwig <dgerwig-@student.42urduliz.com>
2023-11-18 19:10:32 +0100

    Show info about match controls on screen

--- front/src/components/Game/GameMatch/GameMatchSolo.tsx
+++ front/src/components/Game/GameMatch/GameMatchSolo.tsx
@@ -163,6 +163,12 @@ export default function GameMatchSolo(): JSX.Element {
     <WrapperDiv>
       <CenteredLayout>
         <h2 className="title title-1 mb-24">Be ready to challenge our AI 🦾</h2>
+        <h3 className="title title-3">
+          Paddle controls: MOUSE 🖱 // ARROW UP ⬆️ // ARROW DOWN ⬇️
+        </h3>
+        <h3 className="title title-3">
+          Music controls 🔡: HIGH 'h' // LOW 'l' // MUTE 'm' // UNMUTE 'u'
+        </h3>
         <GameCanvasWithAction
           canvasRef={canvasRef}
           background={selectedTheme.backgroundImg}
