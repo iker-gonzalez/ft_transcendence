ba738a89440e922993f0dcc6a9b5493dd8a13d54
Iker González Peña <ikergpe@gmail.com>
2023-11-18 12:53:37 +0100

    Visit friend profile from chat added

--- front/src/components/Chat/ChatMessageAreaHeader.tsx
+++ front/src/components/Chat/ChatMessageAreaHeader.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import MainButton from '../UI/MainButton';
 import RoundImg from '../UI/RoundImage';
+import FriendData from '../../interfaces/friend-data.interface';
+import { useUserFriends, useUserData } from '../../context/UserDataContext';
+import ViewNewUserProfile from '../Friends/ViewNewUserProfile';
+import Modal from '../UI/Modal';
+import { useFlashMessages } from '../../context/FlashMessagesContext';
+import FlashMessageLevel from '../../interfaces/flash-message-color.interface';
 
 const HeaderWrapper = styled.div`
   position: relative; // Add this line
@@ -39,18 +45,61 @@ interface ChatMessageAreaHeaderProps {
   avatarSrc: string;
 }
 
-const ChatMessageAreaHeader: React.FC<ChatMessageAreaHeaderProps> = ({ name, avatarSrc }) => (
-  <HeaderWrapper>
-    <div>
-    <Avatar src={avatarSrc} alt={name} />
-    <Title>{name}</Title>
-    </div>
-    <div>
-      <MainButtonStyled>Play</MainButtonStyled>
-      <MainButtonStyled>Profile</MainButtonStyled>
-      <MainButtonStyled>Block</MainButtonStyled>
-    </div>
-  </HeaderWrapper>
-);
+const ChatMessageAreaHeader: React.FC<ChatMessageAreaHeaderProps> = ({ name, avatarSrc }) => {
+    const [friendProfileToShow, setFriendProfileToShow] =
+    useState<FriendData | null>(null);
+  const [showAddNewFriendFlow, setShowAddNewFriendFlow] =
+    useState<boolean>(false);
 
-export default ChatMessageAreaHeader;
+  const { userFriends, setUserFriends, fetchFriendsList, isFetchingFriends } =
+    useUserFriends();
+
+    const friend = userFriends.find(user => user.username === name) || null;
+
+  const { launchFlashMessage } = useFlashMessages();
+
+  useEffect(() => {
+    fetchFriendsList();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  const onUpdateFriendsList = (
+    newFriendsList: FriendData[],
+    successMessage: string,
+  ): void => {
+    setUserFriends(newFriendsList);
+    setFriendProfileToShow(null);
+    setShowAddNewFriendFlow(false);
+
+    launchFlashMessage(successMessage, FlashMessageLevel.SUCCESS);
+  };
+  
+    return (
+        <HeaderWrapper>
+          <div>
+            <Avatar src={avatarSrc} alt={name} />
+            <Title>{name}</Title>
+          </div>
+          <div>
+            <MainButtonStyled onClick={() => console.log('Play button clicked')}>Play</MainButtonStyled>
+            <MainButtonStyled onClick={() => setFriendProfileToShow(friend)}>Profile</MainButtonStyled>
+            <MainButtonStyled onClick={() => console.log('Block button clicked')}>Block</MainButtonStyled>
+          </div>
+          {friendProfileToShow && (
+            <Modal
+              dismissModalAction={() => {
+                setFriendProfileToShow(null);
+              }}
+              showFullScreen={true}
+            >
+              <ViewNewUserProfile
+                foundUserData={friendProfileToShow}
+                isAlreadyFriend={true}
+                onUpdateFriendsList={onUpdateFriendsList}
+              />
+            </Modal>
+          )}
+        </HeaderWrapper>
+      );
+  };
+  
+  export default ChatMessageAreaHeader;
