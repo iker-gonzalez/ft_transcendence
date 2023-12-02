c22c80b020e945a05897daa005df48b2ae87cdd0
Iker González Peña <ikergpe@gmail.com>
2023-12-02 11:51:58 +0100

    Refactoring fixed

---
+++ front/src/interfaces/chat-group-message.interface.ts
@@ -0,0 +1,9 @@
+export default interface GroupMessage {
+    id: string;
+    roomName: string | undefined;
+    senderIntraId: number | undefined;
+    senderName: string;
+    senderAvatar: string;
+    content: string;
+    timestamp: string;
+  }
