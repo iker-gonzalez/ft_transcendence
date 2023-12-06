3d80139199ce71d67e3d57962e48455779f83af3
Nicolas Gasco <nicolas.gasco92@gmail.com>
2023-12-06 22:43:02 +0100

    Ask for confirmation before blocking a user

--- front/src/components/Chat/ChatMessageAreaHeaderConvoActions.tsx
+++ front/src/components/Chat/ChatMessageAreaHeaderConvoActions.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import DangerButton from '../UI/DangerButton';
 import MainButton from '../UI/MainButton';
 import User from '../../interfaces/chat-user.interface';
@@ -9,6 +9,9 @@ import GroupMessage from '../../interfaces/chat-group-message.interface';
 import FlashMessageLevel from '../../interfaces/flash-message-color.interface';
 import { useFlashMessages } from '../../context/FlashMessagesContext';
 import { nanoid } from 'nanoid';
+import SecondaryButton from '../UI/SecondaryButton';
+import Modal from '../UI/Modal';
+import styled from 'styled-components';
 
 type ChatMessageAreaHeaderConvoActionsProps = {
   user: User;
@@ -17,10 +20,19 @@ type ChatMessageAreaHeaderConvoActionsProps = {
   block: (username: string, intraId: number, block: number) => void;
 };
 
+const ActionsContainerDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+`;
+
 const ChatMessageAreaHeaderConvoActions: React.FC<
   ChatMessageAreaHeaderConvoActionsProps
 > = ({ user, userData, onNewMessage, block }): JSX.Element => {
   const { launchFlashMessage } = useFlashMessages();
+  const [showConfirmationModal, setShowConfirmationModal] =
+    useState<boolean>(false);
 
   return (
     <>
@@ -57,14 +69,54 @@ const ChatMessageAreaHeaderConvoActions: React.FC<
           >
             Challenge
           </MainButton>
-          <DangerButton
-            onClick={() =>
-              block(user.username, user.intraId, user.isBlocked ? 0 : 1)
-            }
-          >
-            {user.isBlocked ? 'Unblock' : 'Block'}
-          </DangerButton>
+          {user.isBlocked ? (
+            <SecondaryButton
+              onClick={() =>
+                block(user.username, user.intraId, user.isBlocked ? 0 : 1)
+              }
+            >
+              Unblock
+            </SecondaryButton>
+          ) : (
+            <DangerButton onClick={() => setShowConfirmationModal(true)}>
+              Block
+            </DangerButton>
+          )}
         </div>
+      )}
+      {showConfirmationModal && (
+        <Modal
+          dismissModalAction={() => {
+            setShowConfirmationModal(false);
+          }}
+        >
+          <h1 className="title-1 mb-8">
+            Do you confirm blocking {user.username}?
+          </h1>
+          <p className="mb-24">
+            Once you block {user.username}, you will not be able to message each
+            other.
+            <br />
+            You will be able to unblock them later.
+          </p>
+          <ActionsContainerDiv>
+            <SecondaryButton
+              onClick={() => {
+                setShowConfirmationModal(false);
+              }}
+            >
+              Cancel
+            </SecondaryButton>
+            <DangerButton
+              onClick={() => {
+                setShowConfirmationModal(false);
+                block(user.username, user.intraId, user.isBlocked ? 0 : 1);
+              }}
+            >
+              Confirm
+            </DangerButton>
+          </ActionsContainerDiv>
+        </Modal>
       )}
     </>
   );
