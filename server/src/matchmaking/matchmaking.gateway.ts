6e01613646bbdbf3330e1aef9684331fb6aba1a6
Nicolas Gasco <nicolas.gasco92@gmail.com>
2023-12-08 18:26:06 +0100

    Remove unused Swagger docs for sockets

--- server/src/matchmaking/matchmaking.gateway.ts
+++ server/src/matchmaking/matchmaking.gateway.ts
@@ -7,11 +7,6 @@ import {
 import { Server } from 'socket.io';
 import { MatchmakingService } from './matchmaking.service';
 import { Socket } from 'socket.io-client';
-// import { AsyncApiPub, AsyncApiSub } from 'nestjs-asyncapi';
-import { NewQueuedUserDto } from './dto/new-queued-user.dto';
-import { NewQueuedUserResponseDto } from './dto/new-queued-user-response.dto';
-import { swaggerAsyncConstants } from '../../config/swagger-async.constants';
-import { UnqueuedUserDto } from './dto/unqueued-user.dto';
 
 @WebSocketGateway({
   namespace: 'matchmaking',
@@ -22,45 +17,11 @@ export class MatchmakingGateway implements OnGatewayDisconnect {
   @WebSocketServer()
   server: Server;
 
-  // @AsyncApiPub({
-  //   channel: swaggerAsyncConstants.matchmaking.endpoints.newUser.channel,
-  //   description:
-  //     swaggerAsyncConstants.matchmaking.endpoints.newUser.description,
-  //   message: {
-  //     payload: NewQueuedUserDto,
-  //   },
-  // })
-  // @AsyncApiSub({
-  //   channel: swaggerAsyncConstants.matchmaking.endpoints.newUserIntraId.channel,
-  //   description:
-  //     swaggerAsyncConstants.matchmaking.endpoints.newUserIntraId.description,
-  //   message: {
-  //     payload: NewQueuedUserResponseDto,
-  //   },
-  // })
   @SubscribeMessage('newUser')
   onNewUser(client: Socket, data: string): Promise<void> {
     return this.matchmakingService.addUserToQueue(this.server, client.id, data);
   }
 
-  // @AsyncApiPub({
-  //   channel: swaggerAsyncConstants.matchmaking.endpoints.unqueueUser.channel,
-  //   description:
-  //     swaggerAsyncConstants.matchmaking.endpoints.unqueueUser.description,
-  //   message: {
-  //     payload: NewQueuedUserDto,
-  //   },
-  // })
-  // @AsyncApiSub({
-  //   channel:
-  //     swaggerAsyncConstants.matchmaking.endpoints.unqueuedUserIntraId.channel,
-  //   description:
-  //     swaggerAsyncConstants.matchmaking.endpoints.unqueuedUserIntraId
-  //       .description,
-  //   message: {
-  //     payload: UnqueuedUserDto,
-  //   },
-  // })
   @SubscribeMessage('unqueueUser')
   onUnqueueUser(client: Socket, data: string): Promise<void> {
     return this.matchmakingService.removeUserFromQueue(
