feed049d59da1c309a39e3196ae74f6901530746
Nicolas Gasco <nicolas.gasco92@gmail.com>
2023-12-09 13:27:29 +0100

    Fix issue with DM length preventing Challenge feature from working

--- server/src/chat/service/chatDM.service.ts
+++ server/src/chat/service/chatDM.service.ts
@@ -169,9 +169,9 @@ export class ChatDMService {
           'userSender or userReceiver does not exist in DB',
         );
 
-      if (content.length > 124 || content.length < 1)
+      if (content.length > 512 || content.length < 1)
         throw new BadRequestException(
-          'Message length must be between 1 and 124 characters',
+          'Message length must be between 1 and 512 characters',
         );
 
       const existingMessage = await this.prisma.directMessage.findUnique({
